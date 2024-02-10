294e100a484fa822e23b779d9470315595c9b7c0
Kimia Saedi <kimias@Kimias-MacBook-Pro.local>
2024-02-10 15:50:32 -0600

    commit

---
+++ window-bit-count-apx/window-bit-count-apx.h
@@ -0,0 +1,49 @@
+#ifndef _WINDOW_BIT_COUNT_APX_
+#define _WINDOW_BIT_COUNT_APX_
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+
+uint64_t N_MERGES = 0; // keep track of how many bucket merges occur
+
+/*
+    TODO: You can add code here.
+*/
+
+typedef struct {
+    // TODO: Fill me.
+} StateApx;
+
+// k = 1/eps
+// if eps = 0.01 (relative error 1%) then k = 100
+// if eps = 0.001 (relative error 0.1%) the k = 1000
+uint64_t wnd_bit_count_apx_new(StateApx* self, uint32_t wnd_size, uint32_t k) {
+    assert(wnd_size >= 1);
+    assert(k >= 1);
+
+    // TODO: Fill me.
+
+    // TODO:
+    // The function should return the total number of bytes allocated on the heap.
+    return 0;
+}
+
+void wnd_bit_count_apx_destruct(StateApx* self) {
+    // TODO: Fill me.
+    // Make sure you free the memory allocated on the heap.
+}
+
+void wnd_bit_count_apx_print(StateApx* self) {
+    // This is useful for debugging.
+}
+
+uint32_t wnd_bit_count_apx_next(StateApx* self, bool item) {
+    // TODO: Fill me.
+    return 0;
+}
+
+#endif // _WINDOW_BIT_COUNT_APX_
